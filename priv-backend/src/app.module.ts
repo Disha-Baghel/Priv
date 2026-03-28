30dfecdc9b2feb1597dac93a9f247fcfbde219bd
Disha Baghel <bagheldisha708@gmail.com>
2026-03-29 00:47:27 +0530

    module imports for application

--- priv-backend/src/app.module.ts
+++ priv-backend/src/app.module.ts
@@ -3,9 +3,28 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ChatModule } from './chat/chat.module';
+import { ConfigModule } from '@nestjs/config';
+import * as Joi from 'joi'
 
 @Module({
-  imports: [PrismaModule, AuthModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true, 
+      validationSchema: Joi.object({
+        NODE_ENV: Joi.string().default('development'),
+        PORT: Joi.string().default(3000),
+        JWT_SECRET: Joi.string().min(16).required(),
+        JWT_EXPRES_IN_SECONDS: Joi.number().default(86400),
+        DATABASE_URL: Joi.string().required(),
+      })
+    }),
+    PrismaModule, 
+    AuthModule, 
+    UsersModule, 
+    ChatModule
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
